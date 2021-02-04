4292248709cc2a19565b7dc80360ef1e31097f5a
manucho <manuel.blanco77@gmail.com>
2021-02-04 11:10:20 -0300

    MyMessage component definition + style

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,7 +1,19 @@
-const MyMessage = () => {
+const MyMessage = ({ message }) => {
+    if (message?.attatchments?.length > 0) {
+        return (
+            <img 
+                src={message.attatchments[0].file}
+                alt="message-attatchment"
+                className="message-image"
+                style={{ float: 'right' }}
+            />
+        )
+    }
     return (
-        <div>
-            My Message
+        <div 
+            className="message" 
+            style={{float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#3B2A50'}}>
+            {message.text}
         </div>
     )
 }
