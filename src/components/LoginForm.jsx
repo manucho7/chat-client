cda892e6458709cb2f962c2e7f395cb63ea376d1
manucho <manuel.blanco77@gmail.com>
2021-02-05 11:46:57 -0300

    LoginForm component + conditional rendering according to localStorage=> username

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import axios from 'axios';
+
+const LogninForm = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+
+        const authObject = { 'Project-ID': "398cbef1-8a67-494f-b276-3a44a07123e9", 'User-Name': username, 'User-Secret': password }
+
+        try {
+            await axios.get('https://api.chatengine.io/chats', { headers: authObject });
+
+            localStorage.setItem('username', username);
+            localStorage.setItem('password', password);
+
+            window.location.reload();
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return (
+        <div className="wrapper">
+            <div className="form">
+                <h1 className="title">Chat Application</h1>
+                <form onSubmit={handleSubmit}>
+                    <input 
+                        type="text" 
+                        value={username} 
+                        onChange={(e) => setUsername(e.target.value)}
+                        className="input"
+                        placeholder="Username"
+                        required
+                    />
+                    <input 
+                        type="text"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        className="input"
+                        placeholder="Password"
+                        required
+                    />
+                    <div align="center">
+                        <button type="submit" className="button">
+                            <span>Start Chatting</span>
+                        </button>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default LogninForm;
